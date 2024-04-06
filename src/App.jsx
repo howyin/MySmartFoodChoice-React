ce91165162b43813ae9a292b4cd4b1898f12efe5
Silversouls99 <90676249+Silversouls99@users.noreply.github.com>
2024-04-07 07:35:37 +0800

    Added Contact us and About Us Page
    The pages are not centered. Would need help centering it. Not yet done with both pages. About us needs some more information filled in and Contact Us needs the submission logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import HomeSignInPage from './AuthenticationPage/HomeSignInPage.jsx';
 import HomeSignUpPage from './AuthenticationPage/HomeSignUpPage.jsx';
 import { HomePage } from './LandingPage/HomePage.jsx';
 import ReviewForm from './ReviewPage/ReviewForm.jsx';
+import AboutUs from './Pages/AboutUs.jsx';
+import ContactUs from './Pages/ContactUs.jsx';
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +14,8 @@ function App() {
         <Route path="/SignUp" element={<HomeSignUpPage />} />
         <Route path="/SignIn" element={<HomeSignInPage />} />
         <Route path = "/ReviewForm" element={<ReviewForm />} /> 
+        <Route path = "/AboutUs" element={<AboutUs />} /> 
+        <Route path = "/ContactUs" element={<ContactUs />} /> 
         {/* Add more routes as needed */}
       </Routes>
     </BrowserRouter>
