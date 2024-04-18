7ec12d290ab4e376eb83706af67e65566a531ad2
Liang Luong <66522372+Luong142@users.noreply.github.com>
2024-04-18 16:29:16 +0800

    remove the login and signup
    burn out is real

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomeSignInPage from './AuthenticationPage/HomeSignInPage.jsx';
-import HomeSignUpPage from './AuthenticationPage/HomeSignUpPage.jsx';
 import { HomePage } from './LandingPage/HomePage.jsx';
 import ReviewForm from './ReviewPage/ReviewForm.jsx';
 import AboutUs from './Pages/AboutUs.jsx';
 import ContactUs from './Pages/ContactUs.jsx';
-function App() {
+
+function App() 
+{
+  // remove login and logout
+
+  // add more routes as needed
+  // review database share route
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/SignUp" element={<HomeSignUpPage />} />
-        <Route path="/SignIn" element={<HomeSignInPage />} />
         <Route path = "/ReviewForm" element={<ReviewForm />} /> 
         <Route path = "/AboutUs" element={<AboutUs />} /> 
         <Route path = "/ContactUs" element={<ContactUs />} /> 
--- src/LandingPage/Video.jsx
+++ src/LandingPage/Video.jsx
@@ -8,8 +8,8 @@ const Video = () => {
             <iframe 
                     height="500"
                     width="800"
-                    src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
-                    title="Rick roll"
+                    src="https://www.youtube.com/embed/VPvVD8t02U8?si=zflbCkdAebwl7vSV" 
+                    title="Flutter video"
                     frameborder="0" 
                     allow="accelerometer; autoplay; clipboard-write; 
                     encrypted-media; 
