31db9e6a123762b78ed19925f17c58b265766066
FatalGrin <78714201+FatalGrin@users.noreply.github.com>
2024-04-26 10:25:32 +0800

    remove some extra lines

--- src/Pages/GuestRecordMeal.jsx
+++ src/Pages/GuestRecordMeal.jsx
@@ -6,9 +6,9 @@ import photoIcon from '../assets/PhotoIcon.jpg'; // Import the photo icon
 function GuestRecordMeal() {
   return (
     <div className="record-meal-container">
-      {/* Photo upload section */}
+      
       <div className="photo-upload-section">
-        {/* Placeholder text for photo upload */}
+        
         <label htmlFor="photo-upload" className="upload-label">
           <img src={photoIcon} alt="Photo Icon" className="photo-icon" />
           Please upload the food photo:
@@ -16,7 +16,7 @@ function GuestRecordMeal() {
         <input type="file" id="photo-upload" accept="image/*" className="upload-input" />
       </div>
 
-      {/* Back button to go back to the guest page */}
+      
       <div className="back-button">
         <Link to="/guest">Back to Guest Page</Link>
       </div>
