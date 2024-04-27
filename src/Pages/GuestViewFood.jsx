f59d5c40315db3186919aa376fa5864125cb2b88
FatalGrin <78714201+FatalGrin@users.noreply.github.com>
2024-04-28 00:48:52 +0800

    View Meal History

---
+++ src/Pages/GuestViewFood.jsx
@@ -0,0 +1,13 @@
+// GuestViewMeal.jsx
+import React from 'react';
+
+function GuestViewMeal() {
+  return (
+    <div>
+      <h2>Guest View Food Recipes Page</h2>
+      {}
+    </div>
+  );
+}
+
+export default GuestViewMeal;
