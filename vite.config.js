260f54d31d593d40a2bbd92b28097355f5ee637b
howyin <howyinkong@gmail.com>
2024-05-11 08:15:26 +0800

    Vite config issue

--- vite.config.js
+++ vite.config.js
@@ -4,28 +4,5 @@ import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      open: true,
-      gzipSize: true
-    })
-  ],
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules/react')) return 'react';
-          if (id.includes('node_modules/react-dom')) return 'react-dom';
-          if (id.includes('firestore/dist/index.esm2017.js')) return 'firestore';
-          if (id.includes('chart.js')) return 'chartjs';
-          // Other conditions...
-          return 'vendor-other';
-        }
-        
-      }
-    },
-    chunkSizeWarningLimit: 800
-  }
-});
+  plugins: [react()],
+})
